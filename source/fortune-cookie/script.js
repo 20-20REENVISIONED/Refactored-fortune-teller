4b41fe3e9c712fbed92793276010bc2b686a815d
Bill <bluff.cyclops_0c@icloud.com>
2024-06-09 01:27:56 -0700

    fix: typo

--- source/fortune-cookie/script.js
+++ source/fortune-cookie/script.js
@@ -222,7 +222,7 @@ document.body.addEventListener("click", async function (event) {
     });
 
     cookieWrapper.classList.add("cracked");
-    fortuneText["data-18n"] = getRandomFortune();
+    fortuneText["data-i18n"] = getRandomFortune();
     fallLeft();
 
     await wait(1000);
