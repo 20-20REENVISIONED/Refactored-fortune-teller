4975a8dc2861f3a35b49c0b6a855c1196de22db4
DongyangLi6816 <dongyangli6816@gmail.com>
2024-06-09 20:05:18 -0700

    Added some comments for feedback page

--- source/feedback-page/script.js
+++ source/feedback-page/script.js
@@ -1,14 +1,24 @@
+/**
+ * Event listener for two buttons and the text box
+ */
 document.addEventListener("DOMContentLoaded", function () {
   var feedbackButton = document.getElementById("feedback-button");
   var clearButton = document.getElementById("clear-button");
   var feedbackTextbox = document.getElementById("feedback-textbox");
 
+  /**
+   * If user click the submit button, it will redirect users to this page agian
+   * and wipe out everything in the text box
+   */
   if (feedbackButton) {
     feedbackButton.addEventListener("click", function () {
-      window.location.href = "index.html"; // Change this URL to your feedback page URL
+      window.location.href = "index.html";
     });
   }
 
+  /**
+   * If user click the clear text button, it will wipe out everything from textbox
+   */
   if (clearButton) {
     clearButton.addEventListener("click", function () {
       feedbackTextbox.value = "";
@@ -16,6 +26,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+/**
+ * When user click the submit button, it will trigger a pop-up with the thank you message.
+ */
 document.addEventListener("DOMContentLoaded", function () {
   var feedbackButton = document.getElementById("feedback-button");
   var feedbackTextbox = document.getElementById("feedback-textbox");
